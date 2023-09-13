f1a30412f90e018c46e4f72295c50843ead3138f
volokhinn <csgomx@mail.ru>
2023-09-13 21:56:04 +0700

    added projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,17 +1,85 @@
+'use client';
 import FileHeader from '@/components/about/FileHeader';
 import ProjectCard from '@/components/ProjectCard';
+import AboutTitle from '@/components/about/AboutTitle';
+
+import projectsData from '../../projects.json';
+
+import Image from 'next/image';
+import { useState } from 'react';
+
 const Projects = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const handleCategoryChange = (category) => {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories((prevCategories) => prevCategories.filter((c) => c !== category));
+    } else {
+      setSelectedCategories((prevCategories) => [...prevCategories, category]);
+    }
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    selectedCategories.every((category) => project.categories.includes(category)),
+  );
+
+  const categories = [
+    {
+      name: 'html',
+      img: require('../../public/icons/technologies/html.svg'),
+    },
+    {
+      name: 'css',
+      img: require('../../public/icons/technologies/css.svg'),
+    },
+    {
+      name: 'js',
+      img: require('../../public/icons/technologies/js.svg'),
+    },
+    {
+      name: 'react',
+      img: require('../../public/icons/technologies/react.svg'),
+    },
+    {
+      name: 'react-native',
+      img: require('../../public/icons/technologies/react.svg'),
+    },
+    {
+      name: 'next',
+      img: require('../../public/icons/technologies/next.svg'),
+    },
+  ];
+
   return (
     <>
-      <div className="border-b-2" style={{ borderColor: '#1E2D3D' }}>
-        <FileHeader title="Projects" link="/" />
-      </div>
-      <div
-        className={`flex flex-wrap gap-x-16 gap-y-16 py-24 px-32 overflow-y-scroll scrollbar scrollbar-w-3 scrollbar-corner-white scrollbar-thumb-slate-500 scrollbar-track-transparent h-[750px]`}>
-        <ProjectCard /> <ProjectCard /> <ProjectCard />
-        <ProjectCard /> <ProjectCard /> <ProjectCard />
-        <ProjectCard /> <ProjectCard /> <ProjectCard />
-        <ProjectCard /> <ProjectCard /> <ProjectCard />
+      <div className={`flex`}>
+        <div className={`border-r-2 w-2/12`} style={{ borderColor: '#1E2D3D' }}>
+          <AboutTitle title="projects" />
+          <div className={`p-5`}>
+            {categories.map((category) => (
+              <div key={category} className={`flex gap-5 items-center mb-6 text-[16px]`}>
+                <input
+                  type="checkbox"
+                  checked={selectedCategories.includes(category.name)}
+                  onChange={() => handleCategoryChange(category.name)}
+                />
+                <Image src={category.img} alt={category.name} width={25} height={25} />
+                {category.name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={`flex-col`} style={{ width: '100%' }}>
+          <div className="border-b-2" style={{ borderColor: '#1E2D3D' }}>
+            <FileHeader title="Projects" link="/" />
+          </div>
+          <div
+            className={`flex flex-wrap gap-x-16 gap-y-16 py-24 px-32 overflow-y-scroll scrollbar scrollbar-w-3 scrollbar-corner-white scrollbar-thumb-slate-500 scrollbar-track-transparent h-[750px]`}>
+            {filteredProjects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-const ProjectCard = () => {
+const ProjectCard = ({ project }) => {
   return (
     <>
       <div className={`flex flex-col w-96`}>
         <h3 className={`text-[#5565E8] mb-3 font-bold`}>
-          Project 1 <span className={`text-[#607B96] font-medium`}>// _ui-animations</span>
+          Project {project.id}{' '}
+          <span className={`text-[#607B96] font-medium`}>// {project.title}</span>
         </h3>
         <div className={`bg-[#011221] rounded-xl border-[#1E2D3D] border-2 relative`}>
           <Image
             alt="project"
-            src={require('../public/project_img.jpeg')}
+            src={require('../public/' + `${project.image}`)}
             className={` rounded-t-xl h-44 object-cover`}
           />
           <Image
             alt="project"
-            src={require('../public/icons/react.svg')}
+            src={require(`../public/icons/${project.mainCategory}.svg`)}
             className={`absolute top-6 right-6`}
           />
           <div className={`py-7 px-7`}>
-            <p className={`text-[#607B96] mb-7 text-[18px]`}>
-              asdasdasd <br></br> asdasd
-            </p>
-            <Link href="/" className={`text-white bg-[#1C2B3A] py-3 px-4 rounded-xl`}>
+            <p className={`text-[#607B96] mb-7 text-[18px]`}>{project.description}</p>
+            <Link
+              href={project.link}
+              target="blank"
+              className={`text-white bg-[#1C2B3A] py-3 px-4 rounded-xl`}>
               view-project
             </Link>
           </div>
